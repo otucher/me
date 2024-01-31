7129fec3b66c91ec056496eb65188a5f367834c1
Oliver Tucher <oliver.tucher@gmail.com>
2024-01-31 15:44:12 -0800

    Updates

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -7,9 +7,10 @@ import RepoStack from "../lib/repo-stack";
 const throwError = (msg: string) => { throw new Error(msg) }
 
 const app = new cdk.App();
-const repositoryName = "resume";
-new RepoStack(app, "resume-repo", { repositoryName });
-new FargateStack(app, "resume", {
+const repositoryName = process.env.REPOSITORY ?? throwError("$REPOSITORY must be defined");
+const repoStack = new RepoStack(app, "resume-repo", { repositoryName });
+const fargateStack = new FargateStack(app, "resume", {
   repositoryName,
   gitTag: process.env.GIT_TAG ?? throwError("$GIT_TAG must be defined")
 });
+fargateStack.addDependency(repoStack)
