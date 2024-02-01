0da18ea2a471efb9bb7710af27cc93a4514dc818
Oliver Tucher <oliver.tucher@gmail.com>
2024-02-01 11:23:20 -0800

    Add env

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -6,11 +6,16 @@ import RepoStack from "../lib/repo-stack";
 
 const throwError = (msg: string) => { throw new Error(msg) }
 
-const app = new cdk.App();
+const env = {
+  account: process.env.AWS_ACCOUNT_ID ?? throwError("$AWS_ACCOUNT_ID must be defined"),
+  region: process.env.AWS_DEFAULT_REGION ?? throwError("$AWS_DEFAULT_REGION must be defined"),
+}
 const repositoryName = process.env.REPOSITORY ?? throwError("$REPOSITORY must be defined");
+const app = new cdk.App();
 const repoStack = new RepoStack(app, "resume-repo", { repositoryName });
 const fargateStack = new FargateStack(app, "resume", {
   repositoryName,
-  gitTag: process.env.GIT_TAG ?? throwError("$GIT_TAG must be defined")
+  gitTag: process.env.GIT_TAG ?? throwError("$GIT_TAG must be defined"),
+  env,
 });
 fargateStack.addDependency(repoStack)
